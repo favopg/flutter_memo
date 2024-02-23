a38fbf6b7a1398f864f837c5077b7c1257e96994
favopg <favoriteigo0904@gmail.com>
2024-02-24 08:23:31 +0900

    'develop#11対応'

--- app/lib/confirm_list.dart
+++ app/lib/confirm_list.dart
@@ -10,11 +10,32 @@ class ConfirmList extends StatelessWidget {
         appBar: AppBar(
           automaticallyImplyLeading: true,
           centerTitle: true,
-          title: const Text("予定確認メモアプリ"),
+          title: const Text("2024年2月の予定一覧"),
         ),
-        body: Container(
-          height: double.infinity,
-          color: Colors.blue
+        body: SizedBox(
+          //width: double.infinity,
+          child: ListView(
+            children: const <Widget>[
+              ListTile(
+                leading: Icon(Icons.schedule),
+                title: Text('2024年2月24日'),
+                subtitle: Text('囲碁研究会'),
+                trailing: Icon(Icons.mode_edit),
+              ),
+              ListTile(
+                leading: Icon(Icons.schedule),
+                title: Text('2024年2月25日'),
+                subtitle: Text('現代文を読む'),
+                trailing: Icon(Icons.mode_edit),
+              ),
+              ListTile(
+                leading: Icon(Icons.schedule),
+                title: Text('2024年2月26日'),
+                subtitle: Text('残念ながら仕事だよ'),
+                trailing: Icon(Icons.mode_edit),
+              ),
+            ],
+          ),
         ),
     );
   }
