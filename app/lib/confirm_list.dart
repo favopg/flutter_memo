3ad0b8280eaf421b2f464276e0662524b79408f6
favopg <favoriteigo0904@gmail.com>
2024-02-23 14:09:39 +0900

    '#13対応'

--- app/lib/confirm_list.dart
+++ app/lib/confirm_list.dart
@@ -1,19 +1,21 @@
 
 import 'package:flutter/material.dart';
 
-class confirmList extends StatelessWidget {
+class ConfirmList extends StatelessWidget {
+  const ConfirmList({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
+          automaticallyImplyLeading: true,
+          centerTitle: true,
           title: const Text("予定確認メモアプリ"),
         ),
         body: Container(
           height: double.infinity,
           color: Colors.blue
         ),
-      ),
     );
   }
 }
