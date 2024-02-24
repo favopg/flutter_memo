4a5be0d9e564c5f695dd27ad219386939d894063
favopg <favoriteigo0904@gmail.com>
2024-02-25 08:33:04 +0900

    '#20対応'

--- app/lib/confirm_list.dart
+++ app/lib/confirm_list.dart
@@ -12,30 +12,95 @@ class ConfirmList extends StatelessWidget {
           centerTitle: true,
           title: const Text("2024年2月の予定一覧"),
         ),
-        body: SizedBox(
-          //width: double.infinity,
-          child: ListView(
-            children: const <Widget>[
-              ListTile(
-                leading: Icon(Icons.schedule),
-                title: Text('2024年2月24日'),
-                subtitle: Text('囲碁研究会'),
-                trailing: Icon(Icons.mode_edit),
+        body: Column(
+          children: [
+            Row(
+              children: const [
+                Text("予定"),
+                Text("予定２")
+              ],
+            ),
+            Expanded(
+              //width: double.infinity,
+              child: ListView(
+                children: const <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月24日'),
+                    subtitle: Text('囲碁研究会'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月25日'),
+                    subtitle: Text('現代文を読む'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月26日'),
+                    subtitle: Text('残念ながら仕事だよ'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月26日'),
+                    subtitle: Text('残念ながら仕事だよ'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月26日'),
+                    subtitle: Text('残念ながら仕事だよ'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月26日'),
+                    subtitle: Text('残念ながら仕事だよ'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月26日'),
+                    subtitle: Text('残念ながら仕事だよ'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月26日'),
+                    subtitle: Text('残念ながら仕事だよ'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月26日'),
+                    subtitle: Text('残念ながら仕事だよ'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月26日'),
+                    subtitle: Text('残念ながら仕事だよ'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月26日'),
+                    subtitle: Text('残念ながら仕事だよ'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.schedule),
+                    title: Text('2024年2月26日'),
+                    subtitle: Text('残念ながら仕事だよ'),
+                    trailing: Icon(Icons.mode_edit),
+                  ),
+
+                ],
               ),
-              ListTile(
-                leading: Icon(Icons.schedule),
-                title: Text('2024年2月25日'),
-                subtitle: Text('現代文を読む'),
-                trailing: Icon(Icons.mode_edit),
-              ),
-              ListTile(
-                leading: Icon(Icons.schedule),
-                title: Text('2024年2月26日'),
-                subtitle: Text('残念ながら仕事だよ'),
-                trailing: Icon(Icons.mode_edit),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
     );
   }
